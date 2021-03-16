4b4e2ef720ca4a2b21b68783352f8603e06fd5bd
Athanasios Kastoras <thanoskastoras@gmail.com>
2021-03-16 11:06:09 +0200

    Sobel Finished

--- sobel_filter.c
+++ sobel_filter.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAX(a,b) ((a < b)? b: a)
+#define TRHESHOLD(a,max) ((a > max)? max: 0)
 
 
 // https://en.wikipedia.org/wiki/Sobel_operator
@@ -17,31 +17,45 @@ pgm_t *sobel_filter(const pgm_t *image)
 {	
 	const int x_kernel[3][3] = {
 		{-1, 0, 1},
-		{-2, 0, 2},
+		{-5, 0, 5},
 		{-1, 0, 1}
 	};
 	const int y_kernel[3][3] = {
-		{-1,-2,-1},
+		{-1,-5,-1},
 		{0, 0, 0},
-		{1, 2, 1}
+		{1, 5, 1}
 	};
 	int x_sum, y_sum;
-	int diff = 1;
 	const int height = image->height, width = image->width, maxval = image->maxval;
 	pgm_t *new_image = new_pgm_image(width, height, image->maxval);
 
 
 	for (int x = 1; x < height - 1; ++x) {
 		for (int y = 1; y < width - 1; ++y) {
-			
-			x_sum = y_sum = 0;
-			for (int i = -1 * diff; i < diff; ++i) {
-				for (int j = -1 * diff; j < diff; ++j) {
-					x_sum += image->pixels[(x+i) * image->width + (y+j)] * x_kernel[i][j];
-					y_sum += image->pixels[(x+i) * image->width + (y+j)] * y_kernel[i][j];
-				}
-			}
-			new_image->pixels[x * image->width + y] = MAX( sqrt(x_sum*x_sum + y_sum*y_sum), maxval);
+			x_sum = (
+				(x_kernel[0][0] * image->pixels[(x - 1)*width + (y - 1)]) +
+				(x_kernel[0][1] * image->pixels[(x)*width + (y - 1)]) +
+				(x_kernel[0][2] * image->pixels[(x + 1)*width + (y - 1)]) +
+				(x_kernel[1][0] * image->pixels[(x - 1)*width + (y)]) +
+				(x_kernel[1][1] * image->pixels[(x)*width + (y)]) +
+				(x_kernel[1][2] * image->pixels[(x + 1)*width + (y)]) +
+				(x_kernel[2][0] * image->pixels[(x - 1)*width + (y + 1)]) +
+				(x_kernel[2][1] * image->pixels[(x)*width + (y + 1)]) +
+				(x_kernel[2][2] * image->pixels[(x + 1)*width + (y + 1)])
+        	);
+			y_sum = (
+				(y_kernel[0][0] * image->pixels[(x - 1)*width + (y - 1)]) +
+				(y_kernel[0][1] * image->pixels[(x)*width + (y - 1)]) +
+				(y_kernel[0][2] * image->pixels[(x + 1)*width + (y - 1)]) +
+				(y_kernel[1][0] * image->pixels[(x - 1)*width + (y)]) +
+				(y_kernel[1][1] * image->pixels[(x)*width + (y)]) +
+				(y_kernel[1][2] * image->pixels[(x + 1)*width + (y)]) +
+				(y_kernel[2][0] * image->pixels[(x - 1)*width + (y + 1)]) +
+				(y_kernel[2][1] * image->pixels[(x)*width + (y + 1)]) +
+				(y_kernel[2][2] * image->pixels[(x + 1)*width + (y + 1)])
+        	);
+
+			new_image->pixels[x * image->width + y] = TRHESHOLD( sqrt(x_sum*x_sum + y_sum*y_sum), maxval);
 		}
 	}
 	return new_image;
